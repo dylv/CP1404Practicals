f1d38d35bffcdcf2fc768c20df8b7d95ffd4d4b1
dylv <dylan.valmadre@my.jcu.edu>
2019-03-22 14:52:44 +1000

    Update: broken_score and temperatures were refactored to include functions.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -5,14 +5,23 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = int(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif 90 > score >= 50:
-    print("Passable")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Bad")
+
+def find_score(score):
+    if score < 0:
+        return "Invalid Score"
+    elif score > 100:
+        return "Invalid Score"
+    elif 90 > score >= 50:
+        return "Passable"
+    elif score >= 90:
+        return "Excellent"
+    else:
+        return "Bad"
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(find_score(score))
+
+
+main()
--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -3,22 +3,37 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f}".format(celsius))
-    else:
-        print("Invalid option")
+
+def calc_celsius():
+    global celsius, fahrenheit
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def calc_fahrenheit():
+    global fahrenheit, celsius
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+
+
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            calc_celsius()
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            calc_fahrenheit()
+            print("Result: {:.2f}".format(celsius))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+main()
