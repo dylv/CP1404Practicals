e36998ca34698b07ca3783ca842319d338bfb06f
dylv <dylan.valmadre@my.jcu.edu>
2019-03-22 13:20:32 +1000

    Refactor password program to implement two functions.

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,14 +1,26 @@
 """Dylan Valmadre"""
 
-min_length = 10
 
-valid_pw = False
-while not valid_pw:
-    password = input(str("Enter password: "))
-    if len(password) < min_length:
-        valid_pw = False
-        print("Password must contain at least 10 digits.")
-    else:
-        valid_pw = True
-        for char in password:
-            print("*", end=" ")
+def main():
+    min_length = 10
+    valid_pw = False
+    get_password(min_length, valid_pw)
+
+
+def get_password(min_length, valid_pw):
+    while not valid_pw:
+        password = input(str("Enter password: "))
+        if len(password) < min_length:
+            valid_pw = False
+            print("Password must contain at least 10 digits.")
+        else:
+            valid_pw = True
+            display_asterisk(password)
+
+
+def display_asterisk(string):
+    for char in string:
+        print("*", end=" ")
+
+
+main()
